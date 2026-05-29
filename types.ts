e2f5593c96c268c317ec85b709524fbdd67cb0be
khankadir193 <abdulkadirk059@gmail.com>
2026-05-29 21:08:02 +0530

    types

---
+++ types.ts
@@ -0,0 +1,25 @@
+//what are the types:-basically types are tell the computer. what kind of type can hold 
+// the variable. may be it can be any types like string,number,boolean,null,undefine
+
+//string
+let fullName: string = 'abdul kadir khan';
+let city: string = 'gorakhpur';
+//number
+let age:number = 12;
+//boolean
+let isStudent:boolean = false;
+let isMarried:boolean = true;
+
+//null vs undefined
+let data:null = null;
+let value:undefined = undefined;
+
+console.log(fullName);
+console.log(city);
+console.log(age);
+console.log(isStudent);
+console.log(isMarried);
+console.log(data);
+console.log(value);
+
+
