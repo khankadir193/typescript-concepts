964a1664af2fbee96196262e1177aac2e4300798
khankadir193 <abdulkadirk059@gmail.com>
2026-07-14 18:48:47 +0530

    key of vs type of example

--- keyOfvsTypeOfExample.ts
+++ keyOfvsTypeOfExample.ts
@@ -3,6 +3,9 @@ keyof operator:-keyof operator give you the union of all property name (keys) of
 
 typeof:used to get the typeof a variable and object.
 
+Mapped type:-Mapped Types allow us to create a new type by transforming the properties of an 
+existing type. Instead of writing similar types repeatedly, we can generate them automatically.
+
 */
 
 interface User {
@@ -29,3 +32,20 @@ type personType = typeof person;
 let val:personType
 
 console.log(typeof person.name);
+
+//mapped type:-
+
+//// Make all properties optional
+type partialUser = {
+    [K in keyof User]? : User[K];
+}
+
+// Make all properties readonly
+type readOnlyUser = {
+    readonly [K in keyof User] : User[K];
+}
+
+// Note: Partial<User> and Readonly<User> are utility types that do the same thing internally.
+
+
+
